c40e40267a325f9e312e2fa66900d77970f214df
sam <syed.simanta06@gmail.com>
2021-06-27 18:01:43 +0600

    ddd

---
+++ src/components/App.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { User } from './../model/Model';
+import { AuthService } from './../services/AuthService';
+import Login from './Login';
+
+interface AppState {
+  user: User | undefined
+}
+
+export default class App extends Component<{}, AppState> {
+
+    private authService: AuthService = new AuthService()
+
+  render() {
+    return (
+      <div>
+    <Login authService={this.authService}/>
+        
+      </div>
+    )
+  }
+}
---
+++ src/services/AuthService.ts
@@ -0,0 +1,12 @@
+import { User } from './../model/Model';
+
+export class AuthService {
+  public async login(userName: string, password: string):Promise<User | undefined> {
+      if (userName === 'user' && password === '1234') {
+        return {
+          userName: userName,
+          email: 'some@gmail.com'
+        }
+      }
+  }
+}
